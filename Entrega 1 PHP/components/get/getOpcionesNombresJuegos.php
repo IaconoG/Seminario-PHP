2d1bd1d4c58ee5940a0905fcd2c204cff203f94b
iacono gianfranco <iacono.gian13@gmail.com>
2023-04-18 20:43:46 -0300

    creacion get nombre juegos para el datalist

---
+++ Entrega 1 PHP/components/get/getOpcionesNombresJuegos.php
@@ -0,0 +1,26 @@
+<?php 
+  //** Conectamos a la base de datos **/
+  include ('config/conexionBD.php');
+
+  //** Obtenemos los nombres de la tabla juegos */
+  $sql = "SELECT nombre FROM juegos"; // Consulta SQL
+  $resultado = mysqli_query($conexion, $sql); // Ejecuta una consulta en la base de datos
+
+  if (!$resultado) { // Si la consulta no se ejecuta correctamente
+    echo '<script>console.error("Error al ejecutar la consulta: ' .  mysqli_error($conexion) . '")</script>';
+    die();
+  }
+
+  //** Mostramos los nombres en la pagina */
+  $row = 'nombre';
+  while ($juego = mysqli_fetch_assoc($resultado)) {
+    $nombreJuego = $juego[$row];
+    $option = "<option value=$nombreJuego name=$nombreJuego>$nombreJuego</option>";    
+    echo $option;
+  }
+  
+  /** Cerramos la conexion **/
+  mysqli_free_result($resultado); // Libera la memoria asociada al resultado
+  mysqli_close($conexion); // Cierra la conexion a la base de datos
+  
+  
