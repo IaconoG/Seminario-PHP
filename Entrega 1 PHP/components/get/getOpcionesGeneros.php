759c50d53b8695c1fa2ab6b8b16fda8481e14ffd
iacono gianfranco <iacono.gian13@gmail.com>
2023-04-18 20:43:04 -0300

    cambio en la consulta y add liberacion memoria

--- Entrega 1 PHP/components/get/getOpcionesGeneros.php
+++ Entrega 1 PHP/components/get/getOpcionesGeneros.php
@@ -3,22 +3,22 @@
   include 'config/conexionBD.php';
 
   // //** Obtenemos los datos de la base de datos **//
-  $sql = "SELECT * FROM  generos";
-  $resultado = mysqli_query($conexion, $sql);
+  $sql = "SELECT nombre FROM  generos"; // Consulta SQL
+  $resultado = mysqli_query($conexion, $sql); // Ejecuta una consulta en la base de datos
 
-  if (!$resultado) {
+  if (!$resultado) { // Si la consulta no se ejecuta correctamente
     echo '<script>console.error("Error al ejecutar la consulta: ' .  mysqli_error($conexion) . '")</script>';
     die();
   }
 
   //** Mostramos los datos en la pagina **//
   $key = 'nombre';
-  while ($fila = mysqli_fetch_assoc($resultado)) { // obtiene una fila de resultados como una matriz asociativa
-    $genero = $fila[$key];
-    $option = "<option value='$genero' name='$genero'>$genero</option>";
+  while ($genero = mysqli_fetch_assoc($resultado)) { // obtiene una fila de resultados como una matriz asociativa
+    $nombreGenero = $genero[$key];
+    $option = "<option value='$nombreGenero' name='$nombreGenero'>$nombreGenero</option>";
     echo $option;
   }
 
   /** Cerramos la conexion **/
-  mysqli_close($conexion);
-?>
+  mysqli_free_result($resultado); // Libera la memoria asociada al resultado
+  mysqli_close($conexion); // Cierra la conexion a la base de datos
--- Entrega 1 PHP/components/get/getOpcionesPlataformas.php
+++ Entrega 1 PHP/components/get/getOpcionesPlataformas.php
@@ -2,24 +2,23 @@
   //** Conectamos a la base de datos **//
   include 'config/conexionBD.php';
 
-
   //** Obtenemos los datos de la base de datos **//
-  $sql = "SELECT * FROM plataformas";
-  $resultado = mysqli_query($conexion, $sql);
+  $sql = "SELECT nombre FROM plataformas"; // Consulta SQL
+  $resultado = mysqli_query($conexion, $sql); // Ejecuta una consulta en la base de datos
 
-  if (!$resultado) {
+  if (!$resultado) { // Si la consulta no se ejecuta correctamente
     echo '<script>console.error("Error al ejecutar la consulta: ' .  mysqli_error($conexion) . '")</script>';
     die();
   }
 
   //** Mostramos los datos en la pagina **//
   $key = 'nombre';
-  while ($fila = mysqli_fetch_assoc($resultado)) { // obtiene una fila de resultados como una matriz asociativa
-    $plataforma = $fila[$key];
-    $option = "<option value='$plataforma' name='$plataforma'>$plataforma</option>";
+  while ($plataforma = mysqli_fetch_assoc($resultado)) { // obtiene una fila de resultados como una matriz asociativa
+    $nombrePlataforma = $plataforma[$key];
+    $option = "<option value='$nombrePlataforma' name='$plataforma'>$nombrePlataforma</option>";
     echo $option;
   }
 
   /** Cerramos la conexion **/
-  mysqli_close($conexion);
-?>
+  mysqli_free_result($resultado); // Libera la memoria asociada al resultado
+  mysqli_close($conexion); // Cierra la conexion a la base de datos
